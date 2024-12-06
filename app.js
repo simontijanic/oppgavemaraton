749c554ba134ff3fb72a9d26162d3be6e22808e0
simontijanic <simontijanicerkul@gmail.com>
2024-12-06 10:43:42 +0100

    hey

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const routes = require('./routes/default');
+
 app.use('/api', routes);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.get('/', (req, res) => {
     res.render("index");
