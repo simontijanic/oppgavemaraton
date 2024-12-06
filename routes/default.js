a4210aae0821d8b48662efdc7c850197814599ac
simontijanic <simontijanicerkul@gmail.com>
2024-12-06 11:21:46 +0100

    mongodb

--- routes/default.js
+++ routes/default.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const greeting = require("../models/greeting")
+const greeting = require("../models/greeting");
 
-router.get('/helloworld', async (req, res) => {
-    try {
-        res.redirect(`http://10.12.14.251/helloworld`)
-    } catch (err) {
-        console.log(errr);
-    }
+router.get("/helloworld", async (req, res) => {
+  try {
+    res.redirect(`http://10.12.14.251/helloworld`);
+  } catch (err) {
+    console.log(errr);
+  }
 });
 
-router.get('/mongo-world', async (req, res) => {
-    try {
-        const greetings = await greeting.find()
+router.get("/mongo-world", async (req, res) => {
+  try {
+    const greetings = await greeting.find();
 
-        res.render('mongo', { greetings });
-    } catch (err) {
-        console.log(err);
-    }
+    res.render("mongo", { greetings });
+  } catch (err) {
+    console.log(err);
+  }
 });
-
 
 module.exports = router;
