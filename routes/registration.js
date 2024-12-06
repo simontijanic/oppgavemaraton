95f532d4d5b6724ceed49b1300e534d09d1db8b2
simontijanic <simontijanicerkul@gmail.com>
2024-12-06 12:49:30 +0100

    bcryp

--- routes/registration.js
+++ routes/registration.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const greeting = require("../models/greeting");
 const User = require("../models/user");
