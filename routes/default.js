44fdb23b1df65b82fcd6cd7e36801e1264e28e42
simontijanic <simontijanicerkul@gmail.com>
2024-12-06 09:58:07 +0100

    Merged main into dev

--- routes/default.js
+++ routes/default.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/defaultController');
+const axios = require('axios');
 
-router.get('/helloworld', controller);
+router.get('/helloworld', async (req, res) => {
+    try {
+        const response = await axios.get('http://10.12.14.251/helloworld');
+        res.send(response.data);
+    } catch(err) {
+        console.log(err)
+    }
+});
 
 module.exports = router;
