635df9fd3c87de19e44eed586e4576efd9490abd
simontijanic <simontijanicerkul@gmail.com>
2024-12-06 10:22:19 +0100

    ferdig

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const routes = require('./routes/default');
 app.use('/api', routes);
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
 app.get('/', (req, res) => {
-  res.send('velkommen');
+    res.render("index");
 });
 
 app.listen(3000, () => {
