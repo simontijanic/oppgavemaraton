516a04936b9d08721e36bc70ce27f8b567fd6d44
simontijanic <simontijanicerkul@gmail.com>
2024-12-06 10:04:04 +0100

    fixed api

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const routes = require('./routes/default');
-
 app.use('/api', routes);
 
+app.get('/', (req, res) => {
+  res.send('velkommen');
+});
+
 app.listen(3000, () => {
-    console.log("listening")
-})
+    console.log("listening");
+});
--- routes/default.js
+++ routes/default.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/defaultController');
 const axios = require('axios');
 
 router.get('/helloworld', async (req, res) => {
     try {
         const response = await axios.get('http://10.12.14.251/helloworld');
-        res.send(response.data);
-    } catch(err) {
-        console.log(err)
+        res.send(response.data); 
+    } catch (err) {
+        console.error('Error fetching from external server:', err);
+        res.status(500).json({ error: 'Unable to fetch hello world content' });
     }
 });
 
